f699105f3a8ac3372a393b825e1a3f232aa981c3
Mehdi <mehdi.janfeshan2014@gmail.com>
2023-05-19 20:49:17 +0430

    container added

--- src/components/navabar/Navbar.jsx
+++ src/components/navabar/Navbar.jsx
@@ -1,18 +1,20 @@
 import styled from "./navbar.module.css";
 
-function Navbar(props) {
 
-  console.log(props)
+
+function Navbar(props) {
   return (
-    <div className={styled.header}>
-      <h3>
-        {props.title}
-      </h3>
-      <ul>
-        <li>لیست مقالات</li>
-        <li>ساخت مقاله</li>
-        <li>درباره</li>
-      </ul>
+    <div className={styled.headerWrapper}>
+      <div className="container">
+        <div className={styled.header}>
+          <h3>{props.title}</h3>
+          <ul>
+            <li>لیست مقالات</li>
+            <li>ساخت مقاله</li>
+            <li>درباره</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,6 +4,10 @@ function Home(){
     return(
         <div>
             <Navbar title="سالار بلاگ"/>
+            <div className="container">
+                <h1 style={{textAlign: "right"}}>Home</h1>
+            </div>
+            
         {/* <Footer />  */}
         </div>
         
