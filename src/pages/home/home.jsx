1b4fa40a8cfe3b4165e2146a5abb7f3f912a07ed
Mehdi <mehdi.janfeshan2014@gmail.com>
2023-05-25 20:31:53 +0430

    useState & useEffect

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,43 +1,45 @@
-import { useEffect } from "react";
+import { useEffect,  useState } from "react";
 import Article from "../../components/article/Article";
 import Navbar from "../../components/navabar/Navbar";
 import styled from "./home.module.css";
+
 function Home(){
 
-    let  articles = [
-        {
-            Id: 1,
-            imageURL: "https://cdn.britannica.com/48/179448-138-40EABF32/Overview-New-York-City.jpg",
-            title: "عنوان اول",
-            readingTime: 5
-        },
-        {
-            Id: 2,
-            imageURL: "https://cdn1.epicgames.com/ue/product/Screenshot/r5-1920x1080-b455d4ad246f168613891f5e162dcf97.jpg?resize=1&w=1920",
-            title: "عنوان دوم",
-            readingTime: 3
-        },
-        {
-            Id: 3,
-            imageURL: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/475000/475669-Jersey-City.jpg",
-            title: "عنوان سوم",
-            readingTime: 8
-        },
-        {
-            Id: 4,
-            imageURL: "https://media.architecturaldigest.com/photos/58f918044f42bd463db36a3f/4:3/w_2663,h_1997,c_limit/1%20-%2010%20Greenest%20Cities%20in%20America%20in%202017.jpg",
-            title: "عنوان چهارم",
-            readingTime: 9
-        }
-    ]
-
+   const [articles, setArticles ] = useState([])
+   
     useEffect(()=>{
+        
         // API call
-       
+    
+        setArticles([
+            {
+                Id: 1,
+                imageURL: "https://cdn.britannica.com/48/179448-138-40EABF32/Overview-New-York-City.jpg",
+                title: "عنوان اول",
+                readingTime: 5
+            },
+            {
+                Id: 2,
+                imageURL: "https://cdn1.epicgames.com/ue/product/Screenshot/r5-1920x1080-b455d4ad246f168613891f5e162dcf97.jpg?resize=1&w=1920",
+                title: "عنوان دوم",
+                readingTime: 3
+            },
+            {
+                Id: 3,
+                imageURL: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/475000/475669-Jersey-City.jpg",
+                title: "عنوان سوم",
+                readingTime: 8
+            },
+            {
+                Id: 4,
+                imageURL: "https://media.architecturaldigest.com/photos/58f918044f42bd463db36a3f/4:3/w_2663,h_1997,c_limit/1%20-%2010%20Greenest%20Cities%20in%20America%20in%202017.jpg",
+                title: "عنوان چهارم",
+                readingTime: 9
+            }
+        ])
 
     }, [])
 
-    console.log(111)
 
 
     return(
